3ff22d2cf24fe952aeef5bf05e92dcce65ccb602
pranavhirulkar21 <127311810+pranavhirulkar21@users.noreply.github.com>
2024-05-28 10:42:26 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,7 @@
 #sudo systemctl restart gunicorn
 #!/bin/bash
+
+pip install --upgrade pip
 # Update package list and install system dependencies
 apt-get update
 apt-get install -y build-essential libpq-dev libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev python-openssl git
