20a2c85945c225e94d7066b8d4937edd260af060
pranavhirulkar21 <127311810+pranavhirulkar21@users.noreply.github.com>
2024-05-28 10:39:32 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,7 @@
-sudo systemctl restart gunicorn
+#sudo systemctl restart gunicorn
+#!/bin/bash
+# Update package list and install system dependencies
+apt-get update
+apt-get install -y build-essential libpq-dev libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev python-openssl git
+
+# Any other commands you need to run before starting your application
