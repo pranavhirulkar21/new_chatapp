076c78926e62c9c2a676137abebfea3892d248f2
pranavhirulkar21 <127311810+pranavhirulkar21@users.noreply.github.com>
2024-07-03 16:19:41 +0530

    Update settings.py

--- fundoo/fundoo/settings.py
+++ fundoo/fundoo/settings.py
@@ -100,27 +100,27 @@ CHANNEL_LAYERS = {
 
 # Database
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.mysql',
-#        'NAME': 'chattingapp-database',
-#        'USER': 'zespxfjzci',
-#        'PASSWORD': 'uJ9Qb$U16pgvfQst',
-#        'HOST': 'chattingapp-server.mysql.database.azure.com',
-#        'PORT': '3306',
-#    }
-#}
-
 DATABASES = {
-'default': {
-'ENGINE': 'django.db.backends.mysql',
-'NAME': os.environ.get('Chatapp_DB'),
-'USER': os.environ.get('Chatapp_DBUSER'),
-'PASSWORD': os.environ.get('Chatapp_DBPASSWORD'),
-'HOST': os.environ.get('Chatapp_DBHOST'),
-'PORT': '3306',
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'chattingapp-database',
+        'USER': 'zespxfjzci',
+        'PASSWORD': 'uJ9Qb$U16pgvfQst',
+        'HOST': 'chattingapp-server.mysql.database.azure.com',
+        'PORT': '3306',
+    }
 }
-}
+
+#DATABASES = {
+#'default': {
+#'ENGINE': 'django.db.backends.mysql',
+#'NAME': os.environ.get('Chatapp_DB'),
+#'USER': os.environ.get('Chatapp_DBUSER'),
+#'PASSWORD': os.environ.get('Chatapp_DBPASSWORD'),
+#'HOST': os.environ.get('Chatapp_DBHOST'),
+#'PORT': '3306',
+#}
+#}
 
 
 #sagar kadam
