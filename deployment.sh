714588116df887aae491b2b8c5ca3d68a94d16d8
pranavhirulkar21 <127311810+pranavhirulkar21@users.noreply.github.com>
2024-07-05 17:08:38 +0530

    Create deployment.sh

---
+++ deployment.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cd /home/azureuser/new_chatappsource venv/bin/activate 
+pip3 install -r requirements.txt
+
+cd /home/azureuser/new_chatapp/fundoo
+python3 manage.py makemigrations
+python3.manage.py migrate
